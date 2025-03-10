a002c71ba7ea62778af4ff1ce529a441379fa08f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-10 12:02:35 +0530

    add bg color, logo and text for the loading page

--- lib/pages/loadingPage.dart
+++ lib/pages/loadingPage.dart
@@ -5,6 +5,24 @@ class LoadingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xffD60030),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/logo.png", width: 200, height: 200),
+            const Text(
+              'BloodHope',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Poppins-Bold',
+                  fontSize: 30),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
