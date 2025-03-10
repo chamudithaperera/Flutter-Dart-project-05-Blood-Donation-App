0514f90d0b14294980726aba415c3fbb9f3c67d4
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-10 11:51:39 +0530

    create the loading page and connect it to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blood_donation_app/pages/loadingPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: LoadingPage(),
     );
   }
 }
