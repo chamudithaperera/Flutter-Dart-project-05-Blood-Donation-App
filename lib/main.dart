625198e1f6689083c248096388188a3e73935077
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-10 11:47:27 +0530

    remove all the things and restart the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-import 'package:blood_donation_app/splashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,22 +6,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primaryColor: const Color(0xFFD60030),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFFD60030), 
-        ),
-        fontFamily: 'Poppins', // Set the main font to Poppins
-      ), 
-      home: const SplashScreen(), 
-      debugShowCheckedModeBanner: false, 
-    ); 
-  } 
-} 
-  
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
