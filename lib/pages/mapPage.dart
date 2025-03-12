c6b61a30556c39a95eea65e4b72f1839f3b2728f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-12 15:35:42 +0530

    add appbar to map page

--- lib/pages/mapPage.dart
+++ lib/pages/mapPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_Appbar.dart';
+
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
@@ -7,7 +9,12 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Map'),
+        backgroundColor: const Color(0xffffffff),
+        elevation: 2, // Increases shadow depth
+        shadowColor: Colors.black.withOpacity(0.3),
+        leading: appBarLeading,
+        title: appBarText,
+        actions: appBarAction,
       ),
       body: const Center(
         child: Text(
