027245408384ca1510cd1728977857bfb8e06845
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-14 20:49:25 +0530

    Refactor GoogleMap widget to use StatefulWidget, add AppBar with customizable properties, and enhance layout for improved user experience.

--- lib/google_map.dart
+++ lib/google_map.dart
@@ -1,12 +1,25 @@
+import 'package:blood_donation_app/widgets/main_Appbar.dart';
 import 'package:flutter/material.dart';
 
-class GoogleMap extends StatelessWidget {
+class GoogleMap extends StatefulWidget {
   const GoogleMap({super.key});
 
   @override
+  State<GoogleMap> createState() => _GoogleMapState();
+}
+
+class _GoogleMapState extends State<GoogleMap> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Map Page'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffffffff),
+        elevation: 2, // Increases shadow depth
+        shadowColor: Colors.black.withOpacity(0.3),
+        leading: appBarLeading,
+        title: appBarText,
+        actions: appBarAction,
+      ),
     );
   }
 }
