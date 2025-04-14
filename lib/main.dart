b62d9a38fa088d65c76304c23ca099c363955b92
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-04-14 20:20:07 +0530

    Remove Firebase and Google Maps dependencies from the project, clean up related code in main.dart, and update Gradle and Podfile configurations to reflect these changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:blood_donation_app/pages/loadingPage.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
